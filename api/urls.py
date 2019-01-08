d656d7700807a2aedc68c209aee6bc29eec05d9b
PS <ps.sukhwani@gmail.com>
2019-01-08 23:42:55 +0530

    update api urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from api.views import UserRegistrationViewSet, UserLoginViewSet, UserLogoutViewSet, HomePageViewSet
 
 urlpatterns = [
-    url('sign-up/', UserRegistrationViewSet.as_view(), name='sign-up'),
-    url('login/', UserLoginViewSet.as_view(), name='user-login'),
-    url('logout/', UserLogoutViewSet.as_view(), name='user-logout'),
-    url('', HomePageViewSet.as_view(), name='profile'),
+    url('^sign-up/$', UserRegistrationViewSet.as_view(), name='sign-up'),
+    url('^login/$', UserLoginViewSet.as_view(), name='user-login'),
+    url('^logout/$', UserLogoutViewSet.as_view(), name='user-logout'),
+    url('^$', HomePageViewSet.as_view(), name='profile'),
 ]
