7be37a2b963c0b3ab9e2f4b8b548715c65b7eab5
pssukhwani <ps.sukhwani@gmail.com>
2019-01-12 13:59:31 +0530

    update base settings for heroku

--- qa/settings/base.py
+++ qa/settings/base.py
@@ -136,13 +136,13 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '../media/')
